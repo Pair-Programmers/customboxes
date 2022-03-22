49aea7c602b02bdf8274b74ebc9215a052e85063
Hamza Saqib <mianhamza7262@gmail.com>
2022-03-23 00:02:44 +0500

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id()->unsigned();
             $table->string('name');
-            $table->string('slug')->unique();
+            $table->string('slug', 100)->unique();
             $table->string('email')->unique();
             $table->string('phone')->nullable();
             $table->string('address')->nullable();
