200fa8847f90a7d648a0d435cd7f0fb0f721111f
Hamza Saqib <mianhamza7262@gmail.com>
2022-03-23 00:37:25 +0500

    a

--- database/migrations/2022_03_05_120421_create_industries_table.php
+++ database/migrations/2022_03_05_120421_create_industries_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('industries', function (Blueprint $table) {
             $table->id()->unsigned();
             $table->string('name');
-            $table->string('slug', 750)->unique();
+            $table->string('slug', 255)->unique();
             $table->string('summary')->nullable();
             $table->string('image')->default('banner.png');
             $table->longText('description')->nullable();
