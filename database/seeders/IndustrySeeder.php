aa270e4c6f6e602beb0846d52400ada6244a9021
Hamza Saqib <mianhamza7262@gmail.com>
2022-03-31 22:52:44 +0500

    industry fix

--- database/seeders/IndustrySeeder.php
+++ database/seeders/IndustrySeeder.php
@@ -20,7 +20,7 @@ class IndustrySeeder extends Seeder
         $industry_names = [
             ['CBD Boxes', 'Implementing the right packaging solution is an important part of your CBD product marketing strategy. CustomBoxesUS, a great source for CBD boxes, can bring innovations to your brand and create a distinct look that you need to increase sales. Make the right impression with your customers by delivering promised quality and effectiveness. Contact us!', true],
             ['Cosmetics Boxes',"It's never easy to please the consumers of the cosmetics, for which you will need to custom cosmetic boxes. With us, you can create great goodwill for your brand and can have more buyers with our box printing techniques. Contact us!", true],
-            ['Gift & Festivities Boxes','---'],
+            ['Gift & Festivities Boxes','---', true],
             ['Food & Beverage Boxes',"What entices the food enthusiast to make an instant purchase? It's the packaging, we at CustomBoxes provide the custom food & beverage boxes that can bring the fiesta vibe to your customer's eyes!", true],
             ['Vape Cartridge Boxes', 'Use our FDA Approved Custom Vape Cartridge boxes in order to make sure that your product has a great shelf life. With us, you can customize the packaging of your product and can many customers in a blink of an eye! Contact us!', true],
             ['Jewelry Boxes','---', true],
