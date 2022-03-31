770e4b752ade589d86a541605612164eb95b24ca
Hamza Saqib <mianhamza7262@gmail.com>
2022-04-01 01:45:41 +0500

    sd

--- database/migrations/2022_03_05_120913_create_orders_table.php
+++ database/migrations/2022_03_05_120913_create_orders_table.php
@@ -31,6 +31,9 @@ return new class extends Migration
             $table->string('customer_phone')->nullable();
             $table->longText('customer_note')->nullable();
             $table->string('page_url')->nullable();
+            $table->string('page_url')->nullable();
+            $table->longText('company_name')->nullable();
+            $table->longText('company_web_link')->nullable();
             $table->string('qoute_form')->nullable();
             $table->string('status')->default('Pending');
 
