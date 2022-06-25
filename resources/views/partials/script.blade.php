53d47542551e19a266863461a53e3c2071d115f1
Hamza Saqib <mianhamza7262@gmail.com>
2022-06-25 17:05:43 +0500

    dfg

--- resources/views/partials/script.blade.php
+++ resources/views/partials/script.blade.php
@@ -15,20 +15,20 @@
 <script src="{{ asset('assets/website') }}/js/plugins/sticky-sidebar.js"></script>
 <script src="{{ asset('assets/website') }}/js/plugins/easyzoom.js"></script>
 <script src="{{ asset('assets/website') }}/js/plugins/scrollup.js"></script>
-<script src="{{ asset('assets/website') }}/js/plugins/ajax-mail.js"></script>
+{{-- <script src="{{ asset('assets/website') }}/js/plugins/ajax-mail.js"></script> --}}
 <!-- Main JS -->
 <script src="{{ asset('assets/website') }}/js/main.js"></script>
 
 <!--Start of Tawk.to Script-->
 <script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/5e67702e8d24fc226586c341/default';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/5e67702e8d24fc226586c341/default';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
 </script>
 <!--End of Tawk.to Script-->
