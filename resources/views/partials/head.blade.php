aefcd162dfc1af8277baf1f71ba434efb9811b4b
Hamza Saqib <mianhamza7262@gmail.com>
2022-07-05 21:59:59 +0500

    asd

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -5,6 +5,27 @@
     <meta name="robots" content="index, follow" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/website') }}/images/favicon.png">
+
+    <!-- All CSS is here ============================================ -->
+
+    <link rel="stylesheet" href="{{ asset('assets/website') }}/css/vendor/bootstrap.min.css" media="screen">
+    <link rel="stylesheet" href="{{ asset('assets/website') }}/css/vendor/signericafat.css" media="screen">
+    <link rel="stylesheet" href="{{ asset('assets/website') }}/css/vendor/cerebrisans.css" media="screen">
+    <link rel="stylesheet" href="{{ asset('assets/website') }}/css/vendor/simple-line-icons.css" media="screen">
+    <link rel="stylesheet" href="{{ asset('assets/website') }}/css/vendor/elegant.css" media="screen">
+    <link rel="stylesheet" href="{{ asset('assets/website') }}/css/vendor/linear-icon.css" media="screen">
+    <link rel="stylesheet" href="{{ asset('assets/website') }}/css/plugins/nice-select.css" media="screen">
+    <link rel="stylesheet" href="{{ asset('assets/website') }}/css/plugins/easyzoom.css" media="screen">
+    <link rel="stylesheet" href="{{ asset('assets/website') }}/css/plugins/slick.css" media="screen">
+    <link rel="stylesheet" href="{{ asset('assets/website') }}/css/plugins/animate.css" media="screen">
+    <link rel="stylesheet" href="{{ asset('assets/website') }}/css/plugins/magnific-popup.css" media="screen">
+    <link rel="stylesheet" href="{{ asset('assets/website') }}/css/plugins/jquery-ui.css" media="screen">
+    <link rel="stylesheet" href="{{ asset('assets/website') }}/css/style.css" media="screen">
+
+
     <!-- Global site tag (gtag.js) - Google Analytics -->
     {{-- <script async src="https://www.googletagmanager.com/gtag/js?id=G-R750JWN117"></script>
     <script>
@@ -33,25 +54,6 @@
         })(window, document, 'script', 'dataLayer', 'GTM-TQRWPZB');
     </script>
     <!-- End Google Tag Manager -->
-
-    <!-- Favicon -->
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/website') }}/images/favicon.png">
-
-    <!-- All CSS is here ============================================ -->
-
-    <link rel="stylesheet" href="{{ asset('assets/website') }}/css/vendor/bootstrap.min.css" media="screen">
-    <link rel="stylesheet" href="{{ asset('assets/website') }}/css/vendor/signericafat.css" media="screen">
-    <link rel="stylesheet" href="{{ asset('assets/website') }}/css/vendor/cerebrisans.css" media="screen">
-    <link rel="stylesheet" href="{{ asset('assets/website') }}/css/vendor/simple-line-icons.css" media="screen">
-    <link rel="stylesheet" href="{{ asset('assets/website') }}/css/vendor/elegant.css" media="screen">
-    <link rel="stylesheet" href="{{ asset('assets/website') }}/css/vendor/linear-icon.css" media="screen">
-    <link rel="stylesheet" href="{{ asset('assets/website') }}/css/plugins/nice-select.css" media="screen">
-    <link rel="stylesheet" href="{{ asset('assets/website') }}/css/plugins/easyzoom.css" media="screen">
-    <link rel="stylesheet" href="{{ asset('assets/website') }}/css/plugins/slick.css" media="screen">
-    <link rel="stylesheet" href="{{ asset('assets/website') }}/css/plugins/animate.css" media="screen">
-    <link rel="stylesheet" href="{{ asset('assets/website') }}/css/plugins/magnific-popup.css" media="screen">
-    <link rel="stylesheet" href="{{ asset('assets/website') }}/css/plugins/jquery-ui.css" media="screen">
-    <link rel="stylesheet" href="{{ asset('assets/website') }}/css/style.css" media="screen">
-
+    
     @yield('custom-css')
 </head>
